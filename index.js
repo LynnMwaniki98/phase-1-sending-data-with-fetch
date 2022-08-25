be63db08d3856565baeda096e0f03bbe5755f8c9
Lynn Mwaniki <Lynn.mwaniki@student.moringaschool.com>
2022-08-25 19:00:17 +0300

    slightly done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(nameArg, emailArg){
+    const formData = {
+        "name": nameArg,
+        "email": emailArg,
+
+    };
+    const configureObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+    },
+        body:JSON.stringify(formData)
+    };
+    return fetch('http://localhost:3000/users', configureObject)
+    .then(response => response.json()) 
+    .then(body => addNewInfo(body.id))
+    .catch(error => {
+        let errorMessage=document.createElement("errorMessage")
+        errorMessage.innerHTML='Unauthorized Access';
+        document.body.append(errorMessage)
+    });
+}
